8d1304ab249f278b90901f2fd6a920d80ba3fdce
Shiangan <avarubby85110@gmail.com>
2024-10-30 22:17:17 +0800

    Create UserController.java

---
+++ scr/main/Java/com/example/shopping/controller/UserController.java
@@ -0,0 +1,27 @@
+package com.example.shopping.controller;
+
+import com.example.shopping.model.User;
+import com.example.shopping.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping("/register")
+    public User register(@RequestBody User user) {
+        return userRepository.save(user);
+    }
+
+    @PostMapping("/login")
+    public User login(@RequestBody User user) {
+        User existingUser = userRepository.findByUsername(user.getUsername());
+        if (existingUser != null && existingUser.getPassword().equals(user.getPassword())) {
+            return existingUser;
+        }
+        throw new RuntimeException("Invalid credentials");
+    }
+}
